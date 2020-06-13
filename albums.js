791df793ed2110c146fa6ede158d5252cd6849eb
natalyyemal <nyemal@udesa.edu.ar>
2020-06-12 23:35:38 -0300

    cambios

--- albums.js
+++ albums.js
@@ -1,23 +1,26 @@
-let queryString= location.search;
+let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString); //Obtenemos un objeto literal con toda la info de los parametros en la url
 let idAlbums = queryStringObj.get("id"); //con el método get obtenenemos el valor del término a buscar. En este obtenenemos lo que escribió el usuario en el campo de busqueda cuyo "name" es "search" (name="search").
 
 let proxy = 'https://cors-anywhere.herokuapp.com/';
-let url = proxy + "https://api.deezer.com/chart/0/albums" + idAlbums;
+let url = proxy + "https://api.deezer.com/album/" + idAlbums;
 
 fetch(url)
     .then(function(response){
         return response.json();
     })
 .then(function(datos){
-        //Resolver que hacemos con los datos.
-        console.log(datos);
         let inform = datos.data;
-        let titlee = document.querySelector(".titulo");
-        titlee.innerHTML += "<h1>"+  inform.title + "</h1>";
-        let foto = document.querySelector(".weeknd");
-        foto.src = inform.cover;
-    }) 
+        // console.log(inform);
+        let album = document.querySelector(".titulo");
+        album.innerHTML = datos.title;
+        
+        // let foto = document.querySelector(".foto-albumes");
+        // foto.src = datos.cover_medium;
+        // console.log(foto);
+        // // let foto = document.querySelector(".weeknd");
+        // foto.src = inform.cover;
+    })
 .catch(function(error){
         console.log(error); 
     })
