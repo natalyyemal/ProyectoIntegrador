a22d713bd9be26d1570f050902f513d3ebd07376
valudrechsler <vdrechsler@udesa.edu.ar>
2020-06-15 19:40:30 -0300

    hola

--- todoslostracks.js
+++ todoslostracks.js
@@ -1,3 +1,10 @@
+// let proxy = 'https://cors-anywhere.herokuapp.com/';
+// let url = proxy + 'https://api.deezer.com/chart/0/tracks';
+let queryString = location.search;
+let datos = new URLSearchParams (queryString);
+// let idTrack = datos.get ('id');
+// console.log(idTrack);
+
 let proxy = 'https://cors-anywhere.herokuapp.com/';
 let url = proxy + 'https://api.deezer.com/chart/0/tracks';
 
@@ -8,15 +15,18 @@ fetch(url)
 .then(function(data){
     console.log(data);
     let datos = data.data;
-    let track = document.querySelector(".foto-bandas");
+    let track = document.querySelector(".fotos");
     let tracks = "";
     for(let i=0; i<9; i++){
-        tracks += "<div class = 'fotos'>";
-        tracks += "<a href = 'tracks.html?id=" + datos[i].id + "'" + "</a>";
-        tracks += "<img src='" + datos[i].cover_medium + "' alt= ' " + "'>";
-        tracks += "</div>";
+        // track.innerHTML = "<div class = 'foto-bandas'>";
+        track.innerHTML += '<li class="trackschart">' + "<a href = 'tracks.html?id=" + datos[i].id + "'";
+        track.innerHTML += "<img src='" + datos[i].artist.picture_small + "'>";
+        track.innerHTML += datos[i].title_short + "</a></li>";
+
+        // track.innerHTML += "</div>";
+        // tracks += '<li>' + '<a  href="tracks.html?id='+ datos[i].id + '">'+ datos[i].title_short + '</a></li>'
     }
-    track.innerHTML = tracks
+    // track.innerHTML = tracks;
 })
 .catch(function(error){
     console.log(error);
