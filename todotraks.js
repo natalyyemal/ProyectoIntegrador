567456721452cbcd26d27ab7eb1f47cf2837309f
valudrechsler <vdrechsler@udesa.edu.ar>
2020-06-14 21:18:17 -0300

    hipervinuclo

---
+++ todotraks.js
@@ -0,0 +1,52 @@
+
+// let queryString= location.search;
+// let queryStringObj = new URLSearchParams(queryString); 
+// let idTracks = queryStringObj.get("id"); 
+
+// let proxy = 'https://cors-anywhere.herokuapp.com/';
+// let url =  proxy + "https://api.deezer.com/chart/0/tracks";
+// fetch(url)
+// .then(function(response){
+//     return response.json()
+// })
+
+// .then(function(info){
+//     console.log(info);
+//     let foto = info.data;
+//     let bandas = document.querySelector('.foto-bandas');
+//     let albums = ''; 
+//     for(let i=0; i<9; i++){
+//       albums += '<div class="fotos">';  
+//       albums += '<a href= "tracks.html?id=' + foto[i].id + '"' + '</a>';
+//       albums += '<img src="' + foto[i].picture + '" alt="' +  '">';
+//       albums += '</div>';
+//     }
+//     bandas.innerHTML = albums;
+
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let url = proxy + "https://api.deezer.com/chart/0/tracks";
+
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+
+        let tracks = document.querySelector('.tracks');
+        let resultados = datos.data;
+
+        resultados.forEach(function(track){
+            tracks.innerHTML += '<li>' + '<a  class="hipervinculo" href="tracks.html?id='+ track.id+ '">'+ track.title + '</a></li>' 
+        })
+
+    })
+    .catch(function(error){
+        console.log(error);
+
+    })
