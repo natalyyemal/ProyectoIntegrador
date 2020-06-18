58a5e3e2463311459a7532c78256180c92ae96db
valudrechsler <vdrechsler@udesa.edu.ar>
2020-06-18 15:50:35 -0300

    buscador de todo

--- resultados.js
+++ resultados.js
@@ -4,41 +4,116 @@ window.addEventListener('load', function(){
 })
 let queryString= location.search;
 let queryStringObj = new URLSearchParams(queryString); //Obtenemos un objeto literal con toda la info de los parametros en la url
+
+
+
+let option = queryStringObj.get('option');
 let idArtista = queryStringObj.get("search"); //con el método get obtenenemos el valor del término a buscar. En este obtenenemos lo que escribió el usuario en el campo de busqueda cuyo "name" es "search" (name="search").
-// let option = queryStringObj.get('option');
-// // Mantener en el html la opción seleccionada
+let arrayOpciones = document.querySelectorAll('.option');
 
-// // Mantener en el html la opción seleccionada
-// // 1 capturamos todas los campos input de tipo radio button.
-// let arrayOpciones = document.querySelectorAll('.option');
-// // Recorremos el array de radio buttons y le colocamos el atributo checked al input que coinicde con el valor de "option" que está en la url.
-// arrayOpciones.forEach(function(unaOpcion){
-//     if(option == unaOpcion.value){
-//         unaOpcion.checked = true
-//     }
-// })
+let search = queryStringObj.get('search');
 
-let proxy = 'https://cors-anywhere.herokuapp.com/';
-let url =  proxy + "https://api.deezer.com/search/artist?q=" + idArtista;
+let proxy = "https://cors-anywhere.herokuapp.com/";
+let urltracks = proxy + 'https://api.deezer.com/search/' + option + '?q=' + search;
+
+// let proxy = 'https://cors-anywhere.herokuapp.com/';
+// let url =  proxy + "https://api.deezer.com/search/artist?q=" + idArtista;
 
 
-fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(datos){
-        //Resolver que hacemos con los datos.
-        console.log(datos);
-        let lista = document.querySelector('.lista');
-        let info = datos.data;  
-        info.forEach(function(resultado){
-        lista.innerHTML += '<li class= "resultados">' + '<a href="detail.html?id=' + resultado.id + '">' + resultado.name + '</a></li>';
-        }); 
-    })
-    .catch(function(error){
-        console.log(error);
+// fetch(url)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(datos){
+//         //Resolver que hacemos con los datos.
+//         console.log(datos);
+//         let lista = document.querySelector('.lista');
+//         let info = datos.data;  
+//         info.forEach(function(resultado){
+//         lista.innerHTML += '<li class= "resultados">' + '<a href="detail.html?id=' + resultado.id + '">' + resultado.name + '</a></li>';
+//         }); 
+//     })
+//     .catch(function(error){
+//         console.log(error);
         
-    });
+//     });
+
+    window.addEventListener('load', function() { //este event listener cubre todo lo que se va a buscar
+
+    
+        if(search !== null){
+            // showSpinner() // ejecuta la funcion spinner SOLO cuando busco algo
+        
+            let searchResults = document.querySelector('.display-resultados');
+            //searchResults.innerHTML += 'Resultados de Busqueda...'; --> creo que queda más bello sin esto, pero lo dejo a tu criterio jeje
+        
+            // let detailContainer = document.querySelector('.main-detail-containter');
+            // detailContainer.style = 'padding: 10px 0px;'
+            fetch(urltracks)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(datos){
+                let lista = document.querySelector('.lista')
+                let resultados = datos.data;
+        
+                lista.innerHTML = "";
+        
+                if (option == 'track'){
+                    resultados.forEach(resultado => { 
+                        // lista.innerHTML += '<li class="track-search-list"><a href="generaldetail.html?id=' + resultado.id + '&type='+ resultado.type + '" class="a-song">'+ '<img src="'+ resultado.album.cover + '" class="search-img">'+ '<div class="song-text"><h4 class="text-a">' + resultado.title + ' </h4><p class="text-b">' + resultado.artist.name + '</p></div>'+ '<i class="material-icons">more_horiz</i>' +'</a></li>'
+                        lista.innerHTML += '<li class= "resultados">' + '<a href="detail.html?id="' + resultado.id + '&type='+ resultado.type + '" class="a-song">' + resultado.title + '</a></li>';
+                    });
+                }
+                if (option == "album"){
+                    resultados.forEach(resultado => { 
+                        resultados.forEach(resultado => {
+                            // lista.innerHTML += '<li class="track-search-list"><a href="generaldetail.html?id='+ resultado.id +'&type='+ resultado.type + '" class="a-song">' + '<img src="' +  resultado.cover  + '" class="search-img">'+ '<div class="song-text"><h4 class="text-a">' + resultado.title + '</h4><p class="text-b">'+ resultado.artist.name + '</p></div>' + '<i class="material-icons">keyboard_arrow_right</i>' + '</a></li>';
+
+                            lista.innerHTML += '<li class= "resultados">' + '<a href="detail.html?id="' + resultado.id + '&type='+ resultado.type + '" class="a-song">' + resultado.title + '</a></li>';
+                        });
+                    });
+                }
+                if (option == "artist") {
+                    resultados.forEach(resultado => {
+                        // lista.innerHTML += '<li class="track-search-list"><a href="generaldetail.html?id='+ resultado.id + '&type=' + resultado.type + '" class="a-song">' + '<img src="' + resultado.picture + '" class="rounded-img">' + '<div class="song-text"><h4 class="text-a">' + resultado.name + '</h4></div>' + '<i class="material-icons">keyboard_arrow_right</i>' + '</a></li>';
+
+                        lista.innerHTML += '<li class= "resultados">' + '<a href="detail.html?id="' + resultado.id + '&type='+ resultado.type + '" class="a-song">' + resultado.name + '</a></li>';
+                    });
+                }
+                // hideSpinner(); //oculta el spinner cuando termina de cargar
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+        }
+        
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     // let search = queryStringObj.get('search');
